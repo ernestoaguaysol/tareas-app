1cfd78645a918e920b101b104da77aba4757b777
Ernesto Aguaysol <ernestoaguaysol@gmail.com>
2019-01-31 14:38:33 -0300

    funcion agregar tarea

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from "./components/Navbar";
+import TaskAdd from "./components/TaskAdd";
 import {todo} from "./todo.json";
 
 class App extends Component {
   constructor(){
     super();
     this.state = { todo }
+    this.addTask = this.addTask.bind(this);
   }
   
+  addTask(task){
+    this.setState({
+      todo: [...this.state.todo, task]
+    })
+  }
   
   render() {
     const todo = this.state.todo.map((task, i) => {
@@ -35,11 +42,19 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar contador={ this.state.todo.length } />
-        <img src={logo} className="App-logo" alt="logo" />
+
 
         <div className="container">
           <div className="row mt-4">
-            { todo }
+            <div className="col-md-3">
+              <img src={logo} className="App-logo" alt="logo" />
+              <TaskAdd onAddTask={this.addTask}/>
+            </div>
+            <div className="col-md-9">
+              <div className="row">
+                { todo }
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/TaskAdd.js
+++ src/components/TaskAdd.js
@@ -1,5 +1,82 @@
 import React, { Component } from 'react';
 
 class TaskAdd extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            responsible: '',
+            description: '',
+            priority: 'low'
+        }
+        this.handleInput = this.handleInput.bind(this);        
+        this.handleSubmit = this.handleSubmit.bind(this);        
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.onAddTask(this.state);
+    }
     
+    handleInput(e) {
+        console.log(e.target.value, e.target.name);
+        const { value, name } = e.target;
+        
+        this.setState({
+            [name]: value 
+        });
+        console.log(this.state);
+    }
+
+    render(){
+        return(
+            <div className="card">
+               <form className="card-body" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="title"
+                            onChange={this.handleInput}
+                            className="form-control"
+                            placeholder="Title"
+                            />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="responsible"
+                            className="form-control"
+                            placeholder="Responsible"
+                            onChange={this.handleInput}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Description"
+                            onChange={this.handleInput}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <select
+                            name="priority"
+                            className="form-control"
+                            onChange={this.handleInput}
+                            >
+                            <option>low</option>
+                            <option>medium</option>
+                            <option>high</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Guardar</button>
+               </form>
+            </div>
+        )
+        
+    }
 }
+
+export default TaskAdd;
